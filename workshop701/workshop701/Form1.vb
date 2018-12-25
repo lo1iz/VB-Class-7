53fb1302602cba49da8282268644fc256e0ed183
Tanawat chinprapinporn <tanawatz@hotmail.com>
2018-12-25 15:55:11 +0700

    Completed

---
+++ workshop701/workshop701/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim zodiac() As String = {"ชวด", "ฉลู", "ขาล", "เถาะ", "มะโรง", "มะเส็ง", "มะเมีย", "มะแม", "วอก", "ระกา", "จอ", "กุน"}
+        Dim year As Integer = TextBox1.Text
+        Dim cal = (year + 5) Mod 12
+
+        MessageBox.Show($"พ.ศ. {year} ตรงกับปี: {zodiac(cal)}")
+    End Sub
+End Class
